db602af7ecdcc299059e7ef3e0f9be362e15a8b4
edwinkkh@gmail.com <edwinkkh@gmail.com>
2020-07-13 21:11:35 +0800

    retrieve text from docx

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+const serverless = require('serverless-http');
 const express = require("express");
 const app = express();
 const multer = require("multer");
 const upload = multer();
+const cors = require('cors')
+const fileUpload = require('express-fileupload');
 
 // parsing application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
@@ -12,9 +15,17 @@ app.use(express.json());
 // parsing multidata/form-data
 app.use(upload.array());
 
+// enable files upload
+app.use(fileUpload({
+    createParentPath: true
+}));
+
+// enable cors
+app.use(cors());
+
 const pdfRoutes = require("./routes/pdf");
 app.use("/pdf", pdfRoutes);
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(4000);
+module.exports.handler = serverless(app);
--- routes/pdf.js
+++ routes/pdf.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const fs = require('fs');
+const path = require("path");
+const pdf = require('pdf-parse');
+const mammoth = require("mammoth");
 
-router.get("/", async (req, res) => {
-    res.send('Hello World' + req.query.name);
+router.get("/", (req, res) => {
+  
+    
+    const path2 = path.join(__dirname,"test.docx");
+    
+    mammoth.convertToHtml({path:path2})
+    .then(function(result){
+        var html = result.value; // The generated HTML
+        var messages = result.messages;
+        res.send(html);
+    })
+    .done();
 });
 
 module.exports = router;
